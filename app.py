1ef1f65b58e8cd84b6ffba5bd19bb0c689b28dfb
SamuelGomesRocha <samu.lgom.srocha@gmail.com>
2023-10-23 12:27:30 -0300

    Comentários

--- app.py
+++ app.py
@@ -8,8 +8,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello this is the new version!"
+    return "Olá, mundo! :D"
 
+#Apresenta extratos de um determinado mês
 @app.route('/extratos', methods=['GET'])
 def getExtratosAPI():
     extratos = []
@@ -24,10 +25,12 @@ def getExtratosAPI():
 
 pessoas = {"pessoas":[{"nome":"Fulano", "cpf":"000.000.000-00", "email":"fulano@emailficticio.com"},{"nome":"Ciclana", "cpf":"111.111.111-11", "email":"ciclana@emailficticio.com"},{"nome":"Beltrana", "cpf":"222.222.222-22", "email":"beltrana@@emailficticio.com"}]}
 
+#Busca todas as pessoas
 @app.route('/pessoas', methods=['GET'])
 def getPessoasAPI():
     return json.dumps(pessoas, indent=6)
 
+#Busca pessoas por meio de uma URI com parâmetros
 @app.route('/pessoa', methods=['GET'])
 def getPessoaAPI():
     return json.dumps(getPessoa(request.args.get('index')), indent=6)
