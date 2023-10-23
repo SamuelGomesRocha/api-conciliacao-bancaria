c5e0d7eae35e40a0e72702513f1580f0abda98c0
SamuelGomesRocha <samu.lgom.srocha@gmail.com>
2023-10-23 12:17:37 -0300

    novos métodos

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,request
 import json
 import random
 #from flask_cors import CORS
@@ -10,14 +10,31 @@ app = Flask(__name__)
 def index():
     return "Hello this is the new version!"
 
-@app.route('/extratos')
-def show_descontos():
+@app.route('/extratos', methods=['GET'])
+def getExtratosAPI():
     extratos = []
-    for n in range(10):
+    for n in range(30):
+        dia = str(n+1)
+        if int(dia) < 10:
+            dia = '0'+dia
         extratos.append(
-            {"data": str(random.randint(1, 30)) + '-oct-2023', "valor_lancado": round(random.uniform(0.00, 900.00), 2)})
+            {"data": dia + '-oct-2023', "valor_lancado": round(random.uniform(0.00, 900.00), 2)})
     extratos_json = {"extratos": extratos}
-    return json.dumps(extratos_json)
+    return json.dumps(extratos_json, indent=6)
+
+pessoas = {"pessoas":[{"nome":"Fulano", "cpf":"000.000.000-00", "email":"fulano@emailficticio.com"},{"nome":"Ciclana", "cpf":"111.111.111-11", "email":"ciclana@emailficticio.com"},{"nome":"Beltrana", "cpf":"222.222.222-22", "email":"beltrana@@emailficticio.com"}]}
+
+@app.route('/pessoas', methods=['GET'])
+def getPessoasAPI():
+    return json.dumps(pessoas, indent=6)
+
+@app.route('/pessoa', methods=['GET'])
+def getPessoaAPI():
+    return json.dumps(getPessoa(request.args.get('index')), indent=6)
+
+def getPessoa(index):
+    return pessoas["pessoas"][int(index)]
+
 
 if __name__ == '__main__':
     app.run()
